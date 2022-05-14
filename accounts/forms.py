0e34fa9f004a75392bfcf34bf0e43d2f522fa782
Greg <gregalelov@gmail.com>
2022-05-15 02:32:59 +0300

    added email check

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from .models import *
 
 
@@ -20,6 +21,12 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with this email already exists!")
+        return email  
 
 
 class ChangePasswordForm(UserCreationForm):
